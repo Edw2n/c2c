84f9581d05842ca3e1f56c7310c97c7c3f683b6e
Seonggu Shin <seg3981@gmail.com>
2023-06-08 16:00:08 +0900

    [ListView] - mui/material 라이브러리 다운로드: package.json / yarn.lock에 해당 라이브러리 파일 추가 - SearchView.css: grid-column의 비율이 8%, 90%이던 것을 8%, 92%로 변경 - 별도의 "Like"버튼 / 체크박스 컬럼을 배치 - Like 버튼은 기능 미구현 - 체크박스는 state 관리하도록 함 : 헤더 체크박스를 선택하면 전체 선택이 됨 - 장바구니(add to cart) 버튼을 배치: 기능은 미구현 상태 - 장바구니 버튼 왼쪽에 "선택한 총 금액" 레이블을 배치: 체크박스 state에 따라 해당 row의 price가 합산되어 표시됨
    [ListView- Detail]
    - Detail ListView의 레이아웃과 배치를 잡기 위하여 더미 데이터 하나를 만들어서 배치를 해 봄(추후 변경될 것임)

--- frontend/src/components/DatasetListView.js
+++ frontend/src/components/DatasetListView.js
@@ -1,7 +1,8 @@
 import './DatasetListView.css';
 import '@coreui/coreui/dist/css/coreui.min.css';
-import { DataGrid } from '@mui/x-data-grid';
-import { DataGridCell, HeaderRow } from 'react-data-grid';
+import { CInputGroup, CFormSelect, CButton } from '@coreui/react';
+import { DataGrid, GRID_CHECKBOX_SELECTION_COL_DEF } from '@mui/x-data-grid';
+import { Button, Checkbox } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 
 function DatasetListView({listInfo}) {
@@ -14,10 +15,63 @@ function DatasetListView({listInfo}) {
   //);
 
   const [items, setItems] = useState(listInfo);
+  const [selectedRow, setSelectedRow] = useState([]);
+  const [selectedCheckboxes, setSelectedCheckboxes] = useState([]);
+  const [selectedPrices, setSelectedPrices] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
 
-  const handleViewButtonClick = (row) => {
-    console.log('View button clicked for row:', row);
-    // 여기에 추가
+  // 체크박스 핸들러
+  const handleRowClick = (params) => {
+    setSelectedRow(params.id);
+  };
+  
+  const handleHeaderCheckboxChange = (event) => {
+    const isChecked = event.target.checked;
+    const updatedCheckboxes = {};
+
+    items.forEach((row) => {
+      updatedCheckboxes[row.id] = isChecked;
+    });
+
+    setSelectedCheckboxes(updatedCheckboxes);
+
+    if (isChecked) {
+      const prices = items.map((row) => row.Price);
+      setSelectedPrices(prices);
+      calculateTotalPrice(prices);
+    } else {
+      setSelectedPrices([]);
+      setTotalPrice([]);
+    }
+  };
+  
+  useEffect(() => {
+    const defaultCheckboxes = {};
+    items.forEach((row) => {
+      defaultCheckboxes[row.id] = false;
+    });
+    setSelectedCheckboxes(defaultCheckboxes);
+  }, [items]);
+
+  const handleCheckboxChange = (event, rowId, price) => {
+    setSelectedCheckboxes((prevState) => ({
+      ...prevState,
+      [rowId]: event.target.checked,
+      }));
+
+
+      setSelectedPrices((prevPrices) => {
+        const updatedPrices = event.target.checked
+          ? [...prevPrices, price]
+          : prevPrices.filter((p) => p !== price);
+        calculateTotalPrice(updatedPrices);
+        return updatedPrices;
+      });
+  };
+
+  const calculateTotalPrice = (newSelectedPrices) => {
+    const sum = newSelectedPrices.reduce((acc, price) => acc + price, 0);
+    setTotalPrice(sum);
   };
 
   // column, row 정의
@@ -32,9 +86,67 @@ function DatasetListView({listInfo}) {
     { field: 'SalesCount', headerName: 'Sales Count', width: 80},
     { field: 'MatchedData', headerName: 'Matched Data', width: 90},
     { field: 'Price', headerName: 'Price($)', width: 80},
-    { field: 'PricePerImage', headerName: 'Price per Image($)', width: 110}
-    // { field: 'Likes', headerName: 'Likes', width:  },
+    { field: 'PricePerImage', headerName: 'Price per Image($)', width: 110},
+    { field: 'Like', headerName: '', width:'80',
+      renderCell: (params) => (
+        <strong>
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            style={{fontSize: '0.6rem'}}>
+            Like
+          </Button>
+        </strong>
+      )
+    },
+    {
+      ...GRID_CHECKBOX_SELECTION_COL_DEF,
+      width: '80',
+      headerName: '',
+      renderHeader: () => (
+        <Checkbox
+          color="primary"
+          checked={Object.values(selectedCheckboxes).every((value) => value)}
+          onChange={handleHeaderCheckboxChange}
+        />
+      ),
+      renderCell: (params) => (
+        <Checkbox
+          color="primary"
+          checked={selectedCheckboxes[params.row.id] || false}
+          onChange={(event) => handleCheckboxChange(event, params.row.id, params.row.Price)}
+        />
+      ),
+    }
+
   ];
+
+  const columns_detail = [
+    { field: 'filename', headerName: 'File Name', width: 100},
+    { field: 'QCstate', headerName: 'QC State', width: 70},
+    { field: 'QCscore', headerName: 'QC Score', width: 80},
+    { field: 'Objects', headerName: 'Objects', width: 130},
+    { field: 'roll', headerName: 'Roll', width: 50},
+    { field: 'pitch', headerName: 'Pitch', width: 50},
+    { field: 'yaw', headerName: 'Yaw', width: 50},
+    { field: 'wx', headerName: 'Wx', width: 50},
+    { field: 'wy', headerName: 'Wy', width: 50},
+    { field: 'wz', headerName: 'Wz', width: 50},
+    { field: 'vf', headerName: 'Vf', width: 50},
+    { field: 'vl', headerName: 'Vl', width: 50},
+    { field: 'vu', headerName: 'Vu', width: 50},
+    { field: 'ax', headerName: 'Ax', width: 50},
+    { field: 'ay', headerName: 'Ay', width: 50},
+    { field: 'az', headerName: 'Az', width: 50},
+    { field: 'Price', headerName: 'Price($)', width: 90}
+  ];
+
+  const rows_detail = [
+    { id: 1, filename: 'Snow', QCstate: 'QCed', QCscore: 0.9, Objects: 'Snow', roll: 0.1, pitch: 0.2, yaw: 0.3, wx: 0.4, wy: 0.5, wz: 0.6, vf: 0.7, vl: 0.8, vu: 0.9, ax: 0.1, ay: 0.2, az: 0.3, Price: 100},
+  ]
+
+
 
   const rows = items.map((row) => (
     {id: row.id, Uploader: row.Uploader, Title: row.Title, Description: row.Description,
@@ -47,10 +159,36 @@ function DatasetListView({listInfo}) {
     <div className='ListViewContainerDetail'>
         <DataGrid
           rows={rows}
+          checkboxSelection 
           columns={columns}
           pageSize={10}
+          onRowClick={handleRowClick}
           style={{fontSize: '0.7rem'}}
         />
+
+        <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr', justifyItems: 'end', marginTop: '5px'}}>
+            <span style={{fontSize: '0.8rem', fontWeight: 'bold', gridColumn: '4', justifySelf: 'end', marginTop: '5px'}}>선택한 총 금액 : {totalPrice} $</span>
+            <CButton type="button" color="secondary" className="mb-3" variant="outline" id="button-addon2" 
+              style={{width: '50%', height: '60%', gridColumn: '5', display: 'flex', justifyContent: 'center', alignItems: 'center', background: 'rgb(38, 73, 132)'}}>
+                <span style={{fontSize: '0.7rem', color: 'white', fontWeight: 'bold'}}>Add to Cart</span>
+            </CButton>
+        </div> 
+
+        <div style={{justifyContent: 'start', textAlign: 'left'}}>
+          <h5 style={{fontWeight: 'bold'}}>
+            Detail Info
+          </h5>
+          <DataGrid
+            rows={rows_detail}
+            columns={columns_detail}
+            pageSize={10}
+            disableColumnMenu
+            disableSelectionOnClick
+            hideFooterSelectedRowCount
+            autoHeight
+            style={{fontSize: '0.7rem'}}
+          />
+        </div>
     </div>
   );
 };
